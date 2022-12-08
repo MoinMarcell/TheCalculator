fec89736dd8115a36eb0e428e54c2d57f3db57be
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-12-08 12:23:31 +0100

    idk

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Calculator {
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        BigDecimal bigDecimal1 = new BigDecimal(1);
+        BigDecimal bigDecimal2 = new BigDecimal(0);
+        BigDecimal bigDecimal3 = BigDecimal.valueOf(0.3);
+        BigDecimal bigDecimal4 = BigDecimal.valueOf(0.1);
+
+        System.out.println("Add:" + add(bigDecimal1, bigDecimal2));
+
+        System.out.println("Sub:" + sub(bigDecimal2, bigDecimal2));
+
+        System.out.println("Divide:" + divide(bigDecimal1, bigDecimal2)); // error!
+        System.out.println("Divide:" + divide(bigDecimal2, bigDecimal1));
+        System.out.println("Divide:" + divide(bigDecimal3, bigDecimal4));
+        System.out.println("Divide:" + divide(bigDecimal4, bigDecimal3));
+
+        System.out.println("Multiply:" + multiply(bigDecimal1, bigDecimal2));
+        System.out.println("Multiply:" + multiply(bigDecimal2, bigDecimal1));
+        System.out.println("Multiply:" + multiply(bigDecimal3, bigDecimal4));
+        System.out.println("Multiply:" + multiply(bigDecimal4, bigDecimal3));
+    }
+    public static BigDecimal add(BigDecimal bigDecimal1, BigDecimal bigDecimal2){
+        return bigDecimal1.add(bigDecimal2);
+    }
+
+    public static BigDecimal sub(BigDecimal bigDecimal1, BigDecimal bigDecimal2){
+        return bigDecimal1.subtract(bigDecimal2);
+    }
+
+    public static BigDecimal divide(BigDecimal nominator, BigDecimal denominator){
+        try{
+            return nominator.divide(denominator, MathContext.DECIMAL32);
+        }catch (ArithmeticException e){
+            System.err.println("Division durch 0 nicht möglich: " + e);
+        }
+        return new BigDecimal(0);
+    }
+
+    public static BigDecimal multiply(BigDecimal bigDecimal1, BigDecimal bigDecimal2){
+        return bigDecimal1.multiply(bigDecimal2, MathContext.DECIMAL32);
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,65 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CalculatorTest {
-  
+
+    @Test
+    void add() {
+        //GIVEN
+        BigDecimal bigDecimal1 = new BigDecimal(3);
+        BigDecimal bigDecimal2 = new BigDecimal(6);
+        BigDecimal expected = bigDecimal1.add(bigDecimal2);
+        //WHEN
+        BigDecimal actual = Calculator.add(bigDecimal1, bigDecimal2);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void sub() {
+        BigDecimal bigDecimal1 = new BigDecimal(3);
+        BigDecimal bigDecimal2 = new BigDecimal(6);
+        BigDecimal expected = bigDecimal2.subtract(bigDecimal1);
+        //WHEN
+        BigDecimal actual = Calculator.sub(bigDecimal2, bigDecimal1);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void divide(){
+        BigDecimal nominator = new BigDecimal("0.3");
+        BigDecimal denominator = new BigDecimal("0.1");
+        BigDecimal expected = nominator.divide(denominator, MathContext.DECIMAL32);
+
+        BigDecimal actual = Calculator.divide(nominator, denominator);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void when_denominator_zero_then_return_zero(){
+        BigDecimal nominator = new BigDecimal(4);
+        BigDecimal denominator = new BigDecimal(0);
+        BigDecimal expected = new BigDecimal(0);
+
+        BigDecimal actual = Calculator.divide(nominator, denominator);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void multiply(){
+        BigDecimal bigDecimal1 = new BigDecimal("0.3");
+        BigDecimal bigDecimal2 = new BigDecimal("0.2");
+        BigDecimal expected = bigDecimal1.multiply(bigDecimal2, MathContext.DECIMAL32);
+
+        BigDecimal actual = Calculator.multiply(bigDecimal1, bigDecimal2);
+
+        assertEquals(expected, actual);
+    }
 }
